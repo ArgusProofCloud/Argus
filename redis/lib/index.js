26c240063259429b338625de51daae38143a7fab
Cato van Hooijdonk <cato.van.hooijdonk@hotmail.com>
2022-02-17 22:38:28 +0100

    redis export class

--- redis/lib/index.js
+++ redis/lib/index.js
@@ -1,16 +1,44 @@
 const redis = require("redis");
-port= 6360
-host='127.0.0.1'
 
-const client = redis.createClient(port, host);
+const port= 6363 //in conf file
+const host='127.0.0.1' //in conf file
 
-client.on("error", function(err){
-    console.error("error: ", err)
-});
+class Redis
+{
+    constructor()
+    {
+        const client = redis.createClient(port, host)
 
-client.on("connect", function(err){
-    console.log("Redis connected")
-})
+        client.on("error", function(err){
+            console.log(err)
+        });
 
+        client.on("connect", function(err){
+            console.log("connected")
+        })
+    }
 
-//6360
+    InsertInstr = ()=>
+    {
+        await client.connect()
+        //RPUSH (json to string!)
+        await client.quit()
+    }
+
+    GetInstr = ()=>
+    {
+        await client.connect()
+        //LPOP
+        await client.quit()
+    }
+
+    ReadInstr = ()=>
+    {
+        await client.connect()
+        //lrange
+        await client.quit()
+    }
+
+}
+
+module.exports = Redis
