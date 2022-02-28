e2b226067a3aca4b612b04af813d140ef4e18df0
Mathias Maes <watcherwhale@maes.family>
2022-02-28 21:06:02 +0100

    Hide docker build logs

--- checks/test.sh
+++ checks/test.sh
@@ -7,17 +7,14 @@ for DIR in $DIRS
 do
     echo "#############################################"
     echo "Building flow $DIR"
-    echo "#############################################"
-    docker build -f Dockerfile --build-arg checklist=$DIR -t $DIR .
-    echo "#############################################"
+    docker build -f Dockerfile --build-arg checklist=$DIR -t $DIR . > /dev/null
     echo "Running flow $DIR"
-    echo "#############################################"
     docker run --rm $DIR python /app/oneshot.py $@
 
     if [ "$?" != "0" ];
     then
-    echo "#############################################"
-        echo -e "$DIR flow failed"
+        echo ""
+        echo "/!\\ $DIR flow failed /!\\"
         FAILED=1
     fi
 done
@@ -26,7 +23,7 @@ if [ "$FAILED" == "1" ];
 then
     echo "#############################################"
     echo ""
-    echo "Some flows have failed!"
+    echo "/!\\ Some or all flows have failed! /!\\"
 
     exit 1
 fi
