941ef376514cffc69b652b885edfab0033c0bbf4
Mathias Maes <watcherwhale@maes.family>
2022-03-02 20:44:59 +0100

    Fix linting issue

--- logger/lib/index.js
+++ logger/lib/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const winston = require("winston");
 const morgan = require("morgan");
-const {format} = require("winston");
 
 /**
  * A rolling log file with pre configured loggers.
