6abd40fd891d034b54d4338590bd5487ed07e5bd
arash-nouri-1 <arash.nouri@student.ap.be>
2022-03-09 23:49:12 +0100

    Standard input checks

--- checks/dns/check_dnssec.py
+++ checks/dns/check_dnssec.py
@@ -12,12 +12,9 @@ ExitOK = 0
 ExitWarning = 1
 ExitCritical = 2
 ExitUnknown = 3
+default = "8.8.8.8"
 
-def testdnssec(opts):
-    domain = opts.domain
-    domain = domain.replace("https://", "")
-    domain = domain.replace("http://", "")
-    domain = domain.replace("www.", "")
+def testdnssec(domain):
     if domain:
         try:
             socket.gethostbyname_ex(domain)
@@ -32,7 +29,7 @@ def testdnssec(opts):
                 print('{"name": "DNSSEC", "score": 0, "message": "Unable to resolve %s"}'%domain)
                 sys.exit(ExitUnknown)
 
-        dig_requests = os.popen('dig @%s %s +noall +comments +dnssec'%(opts.dnsserver, domain)).read()
+        dig_requests = os.popen('dig @%s %s +noall +comments +dnssec'%(default, domain)).read()
         if "ad;" in dig_requests:
             ad_flag = 1
         else:
@@ -63,17 +60,8 @@ def testdnssec(opts):
         sys.exit(ExitUnknown)
 
 def main():
-    parser = OptionParser()
-    parser.add_option("-H","--domain", type=str,
-                      dest="domain", help="Domain name for check DNSSEC, for example: -H www.ciencias.ulisboa.pt")
-    parser.add_option("-d","--dnsserver", type=str, default="8.8.8.8", dest="dnsserver",
-                      help="Specify the DNS server you need to use for check DNSSEC, for example: -d 127.0.0.1, default value is 8.8.8.8")
-
-    (opts, args) = parser.parse_args()
-    if not opts.domain:
-        parser.error('{"name": "DNSSEC", "message": "Please, this program requires domain arguments, for example: -H www.ciencias.ulisboa.pt."}')
-
-    testdnssec(opts)
+    domain = sys.argv[1]
+    testdnssec(domain)
 
 if __name__ == '__main__':
     main()
--- checks/ip/geoIP.py
+++ checks/ip/geoIP.py
@@ -1,19 +1,42 @@
 #! /usr/bin/env python3
+import string
 import geoip2.database
 import sys
+import dns.resolver
+import re 
 
-def main(a):
+
+
+def main(domein):
+    try:
+        result = dns.resolver.resolve(domein)
+        for ipval in result:
+            ip = ipval.to_text()
+    except:
+        print("Er werd geen IP gevonden voor dit domein.")
+        sys.exit()
+    
+
     with geoip2.database.Reader('GeoLite2-City.mmdb') as reader:
-        response = reader.city(a)
+        try:
+            response = reader.city(ip)
+        except:
+            print("IP werd niet gevonden in database")
+            sys.exit()
         country = response.country.iso_code
         eu_country = ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "EL", "ES", "FR", "HR", "IT", "CY",
                       "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "UK"]
+        
+        is_eu_land = False
         for i in eu_country:
             if i == country:
-                print(f'{{"name": "geoIP", "score": 10, "message": "Uw website wordt gehost binnen de EU, meer bepaald in {country}"}}')
-                sys.exit()
-        print(f'{{"name": "geoIP", "score": 0, "message": "Uw website wordt niet gehost binnen de EU, meer bepaald in {country}, indien je persoonsgegevens verwerkt via je site kan dit problemen rond o.a. GDPR met zich meebrengen. Indien er geen persoonsgegevens verwerkt worden op je site, dan kan je deze score nuanceren."}}')
+                is_eu_land = True
+        if is_eu_land:
+             print(f'{{"name": "geoIP", "score": 10, "message": "Uw website wordt gehost binnen de EU, meer bepaald in {country}"}}')
+
+        elif not is_eu_land:
+            print(f'{{"name": "geoIP", "score": 0, "message": "Uw website wordt niet gehost binnen de EU, meer bepaald in {country}, indien je persoonsgegevens verwerkt via je site kan dit problemen rond o.a. GDPR met zich meebrengen. Indien er geen persoonsgegevens verwerkt worden op je site, dan kan je deze score nuanceren."}}')
             
 if __name__ == "__main__":
-    a = sys.argv[1]
-    main(a)
+    domein = sys.argv[1]
+    main(domein)
