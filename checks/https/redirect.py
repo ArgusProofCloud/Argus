08fb3ccc1924166f0d8a8f0c692bbb81ed3f5594
arash-nouri-1 <arash.nouri@student.ap.be>
2022-03-13 12:28:40 +0100

    redirect check fix

--- checks/https/redirect.py
+++ checks/https/redirect.py
@@ -1,25 +1,24 @@
 #! /usr/bin/env python3
 
 from urllib import request
+import requests
 import sys
+
+import urllib3
 
 DOMAIN = sys.argv[1]
 url = 'http://' + DOMAIN
 HTTPS = "https"
-
+urllib3.disable_warnings()
 
 try:
-    response = request.urlopen(url)
+    res = requests.get(url, verify=False)
 except:
-    try:
-        url = 'http://www.' + DOMAIN
-        response = request.urlopen(url)
-    except:
-        print("Dit domein bestaat niet.", file=sys.stderr)
-        sys.exit()
+    print("Dit domein bestaat niet.", file=sys.stderr)
 
-new_url = response.geturl()
-if HTTPS in new_url:
-    print(f'{{"name": "redirect", "score": 10, "message": Alle potentieel onveilige verkeer via {url}, wordt veilig doorgestuurd naar {new_url}"}}')
 else:
-    print(f'{{"name": "redirect", "score": 0, "message": "Uw website {url} is bereikbaar via het onbeveiligde http, dit betekent dat in bepaalde gevallen bezoekers onversleuteld gegevens naar uw website kunnen doorsturen. Misschien kan u een redirectie naar https opzetten?"}}')
+    new_url = res.url
+    if HTTPS in new_url:
+        print(f'{{"name": "redirect", "score": 10, "message": Alle potentieel onveilige verkeer via {url}, wordt veilig doorgestuurd naar {new_url}"}}')
+    else:
+        print(f'{{"name": "redirect", "score": 0, "message": "Uw website {url} is bereikbaar via het onbeveiligde http, dit betekent dat in bepaalde gevallen bezoekers onversleuteld gegevens naar uw website kunnen doorsturen. Misschien kan u een redirectie naar https opzetten?"}}')
