1cfb0524d330422c9db2577b92779ec0d3e4373d
arash-nouri-1 <arash.nouri@student.ap.be>
2022-03-18 12:06:25 +0100

    Update protocol.py

--- checks/https/protocol.py
+++ checks/https/protocol.py
@@ -15,7 +15,7 @@ except:
 else:
     versie = sslSocket.version()
     if "TLSv1.2" in versie or "TLSv1.3" in versie:
-        print(f'{{"name": "protocol", "score": 10, "message": "Uw domein {DOMAIN} heeft versie {versie}."}}')
+        print(f'{{"name": "protocol", "score": 10, "message": "Uw domein {DOMAIN} gebruikt versie {versie}."}}')
     else:
-        print(f'{{"name": "protocol", "score": 0, "message": "Uw domein {DOMAIN} heeft versie {versie}." }}')
+        print(f'{{"name": "protocol", "score": 0, "message": "Uw domein {DOMAIN} gebruikt versie {versie}." }}')
     sslSocket.close()
