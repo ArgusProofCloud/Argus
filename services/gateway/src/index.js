5e47614402d10ea4be52b5c8db3ab2bc4c2b7098
Mathias Maes <watcherwhale@maes.family>
2022-03-19 11:45:23 +0100

    Move modules & Update gateway and sequencer

---
+++ services/gateway/src/index.js
@@ -0,0 +1,54 @@
+const process = require("process");
+const { v4: uuid } = require("uuid");
+const createService = require("service");
+
+const service = createService("sequencer", "/api/v1");
+const { logger, redis, router } = service;
+
+router.post("/request", async (req, res) => {
+
+    const request = req.body;
+
+    if(!("tracker" in request))
+    {
+        request.tracker = uuid();
+    }
+
+    const promises = [];
+
+    for(const i in request.domains)
+    {
+        const domain = request.domains[i];
+
+        for(const j in request.checklists)
+        {
+            const checklist = request.checklists[j];
+            const job = {
+                id: checklist,
+                tracker: request.tracker,
+                domain: domain
+            };
+            promises.push(redis.insert("jobs:" + checklist, job));
+        }
+    }
+
+    await Promise.all(promises);
+    res.status(201).send({status: 201, message: "Checks have succesfully been requested", tracker: request.tracker});
+});
+
+router.get("/poll", async (req, res) => {
+    const results = await redis.popEmpty("results");
+    if(!results)
+    {
+        res.status(200).contentType("application/json").send([]);
+        return;
+    }
+
+    res.status(200).contentType("application/json").send(results.map(x => JSON.parse(x)));
+});
+
+router.get("/checklists", async (req, res) => {
+    res.status(501).send();
+});
+
+service.start();
