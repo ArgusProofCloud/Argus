9c46318fbffdacb5971918c4152afffbea17acfb
Mathias Maes <watcherwhale@maes.family>
2022-03-23 13:36:11 +0100

    Changed Redis module and enforce mTLS

--- services/service/lib/web.js
+++ services/service/lib/web.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const https = require("https");
 const process = require("process");
 const express = require("express");
 const LogFile = require("logfile");
@@ -17,6 +19,17 @@ module.exports = {
         // Register logging middleware
         app.use(logFile.createMiddleware());
 
+        app.use((req, res, next) => {
+
+            if(!req.client.authorized)
+            {
+                res.status(401).send({status: 401, msg: "Unauthorized, invalid client certificate.", reason: req.client.authorizationError})
+                return;
+            }
+
+            next();
+        });
+
         // Register body parser middleware
         app.use(express.json());
 
@@ -28,7 +41,16 @@ module.exports = {
             res.status(404).send({status: 404, message: "This route does not exist!"});
         });
 
-        return app.listen(process.env.PORT || 3000, () => {
+        return https.createServer({
+
+            cert: fs.readFileSync(process.env.CERT_PATH || "cert.crt"),
+            key: fs.readFileSync(process.env.KEY_PATH || "cert.key"),
+
+            ca: fs.readFileSync(process.env.CA_PATH || "ca.crt"),
+            requestCert: true,
+            rejectUnauthorized: false
+
+        }, app).listen(process.env.PORT || 3000, () => {
             logger.info(`Server started on port ${process.env.PORT || 3000}.`);
         });
     }
