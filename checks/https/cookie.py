e77e2758bfe66465fec04bf628ecdc6db7c4d2e5
arash-nouri-1 <arash.nouri@student.ap.be>
2022-03-30 15:09:31 +0200

    Create cookie.py

---
+++ checks/https/cookie.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import sys
+
+driver = webdriver.Firefox(executable_path=r'C:\Users\arash\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe')
+
+DOMAIN = sys.argv[1]
+url = 'http://' + DOMAIN
+
+driver.get(url)
+cookies = driver.get_cookies()
+domeinen = []
+for array in cookies:
+    domeinen.append(array.get("domain"))
+
+print(domeinen)
+
+for domein in domeinen:
+    if not DOMAIN in domein:
+        print(f'{{"name": "cookie", "score": 0, "message": "Uw website {url} maakt gebruik van third party cookies {domein}."}}')
+        exit()
+print(f'{{"name": "cookie", "score": 10, "message": "Uw website {url} maakt GEEN gebruik van third party cookies."}}')
