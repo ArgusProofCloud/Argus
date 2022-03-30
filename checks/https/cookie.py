9597cd3ce00f207ca1dd454d2192388dc46e7f47
arash-nouri-1 <arash.nouri@student.ap.be>
2022-03-30 13:38:10 +0200

    Create cookie check

---
+++ checks/https/cookie.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import sys
+
+driver = webdriver.Firefox(executable_path="./checks/https/geckodriver-v0.30.0-linux64/geckodriver")
+
+DOMAIN = sys.argv[1]
+url = 'http://' + DOMAIN
+
+driver.get(url)
+cookies = driver.get_cookies()
+domeinen = []
+
+for array in cookies:
+    domeinen.append(array.get("domain"))
+
+print(domeinen)
+
+for domein in domeinen:
+    if not DOMAIN in domein:
+        print(f'{{"name": "cookie", "score": 0, "message": "Uw website {url} maakt gebruik van third party cookies."}}')
+        exit()
+print(f'{{"name": "cookie", "score": 10, "message": "Uw website {url} maakt GEEN gebruik van third party cookies."}}')
