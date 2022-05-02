4c24668a32207c74ae27ef17e9c6f919e72ca0ac
Mathias Maes <watcherwhale@maes.family>
2022-05-02 20:05:28 +0200

    Fix cookie check

--- checks/cookie/third_party.py
+++ checks/cookie/third_party.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import time
 
 from selenium import webdriver
@@ -11,33 +12,61 @@ if SELENIUM_URL is None or SELENIUM_URL == "":
     print("{}")
     exit()
 
-options = webdriver.ChromeOptions()
-options.add_argument("--incognito")
-options.set_capability("loggingPrefs", {'performance': 'ALL'})
-driver = webdriver.Remote(SELENIUM_URL + '/wd/hub', options=options)
+def checkCookies(driver, target):
+    """Test if DNSSEC is enabled for a specific domain.
 
-DOMAIN = sys.argv[1]
-url = 'http://' + DOMAIN
+    Args:
+        driver (selenium.webdriver.Remote): The selenium web driver
+        target (str): The target to check
+    """
 
-driver.get(url)
+    url = 'http://' + target
 
-time.sleep(5)
+    driver.get(url)
 
-cookies = driver.get_cookies()
+    time.sleep(5)
 
-driver.close()
-driver.quit()
+    cookies = driver.get_cookies()
 
-domeinen = []
-namen = []
+    domeinen = []
+    namen = []
 
-for array in cookies:
-    namen.append(array.get("name"))
-    domeinen.append(array.get("domain"))
+    for array in cookies:
+        namen.append(array.get("name"))
+        domeinen.append(array.get("domain"))
 
-for domein in domeinen:
-    if DOMAIN not in domein and domein not in DOMAIN:
-        print('{{"name": "third party", "score": 0, "message": "Uw website %s maakt gebruik van third party cookies %s.}}' % (url, domein))
-        exit()
+    for domein in domeinen:
+        if DOMAIN not in domein and domein not in DOMAIN:
+            print(json.dumps({
+                "name": "third party",
+                "score": 0,
+                "message": "The website loads third party cookies without consent."
+            }))
+            return
 
-print('{{"name": "third party", "score": 10, "message": "Uw website %s maakt GEEN gebruik van third party cookies.}}' % (url))
+    print(json.dumps({
+        "name": "third party",
+        "score": 10,
+        "message": "The website does not load third party cookies."
+    }))
+
+def main():
+    """Main function
+    """
+
+    target = sys.argv[1]
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--incognito")
+    options.set_capability("loggingPrefs", {'performance': 'ALL'})
+    driver = webdriver.Remote(SELENIUM_URL + '/wd/hub', options=options)
+
+    try:
+        checkCookies(driver, target)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
