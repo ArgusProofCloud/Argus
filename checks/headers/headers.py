7c54f3bd66c6395ac5216479a1b6b2d6b1488cd0
Mathias Maes <watcherwhale@maes.family>
2022-05-09 19:00:30 +0200

    Better checks

--- checks/headers/headers.py
+++ checks/headers/headers.py
@@ -120,7 +120,7 @@ def checkHeaders(headers: dict, patterns: dict) -> list:
                     "name": headerKey,
                     "score": 0,
                     "message": f"{headerKey} shoud be absent.",
-                    "value": headers[headerKey],
+                    "value": [headers[headerKey]],
                     "description": "headers"
                 })
             elif not valueTest and pattern['present'] is None:
@@ -136,7 +136,7 @@ def checkHeaders(headers: dict, patterns: dict) -> list:
                     "name": headerKey,
                     "score": 0,
                     "message": "Header is not set up correctly."
-                    "value": headers[headerKey],
+                    "value": [headers[headerKey]],
                     "description": "headers"
                 })
 
